d5987ceb07283bdaf126bc3e68e70ac0f13d26a1
Christopher Wavrin <wavrin@gmail.com>
2014-04-03 11:10:33 -0500

    added markdown support

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,24 @@ module ApplicationHelper
   def current_user
     User.find(session[:user_id]) if session[:user_id]
   end
+  
+  def markdown(text)
+    render_options = {
+      filter_html:     true,
+      hard_wrap:       true, 
+      link_attributes: { rel: 'nofollow' }
+    }
+    renderer = Redcarpet::Render::HTML.new(render_options)
+
+    extensions = {
+      autolink:           true,
+      fenced_code_blocks: true,
+      lax_spacing:        true,
+      no_intra_emphasis:  true,
+      strikethrough:      true,
+      superscript:        true
+
+    }
+    Redcarpet::Markdown.new(renderer, extensions).render(text).html_safe
+  end
 end
