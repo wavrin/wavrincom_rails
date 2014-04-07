2146d02c8c1cd036ad1c95579b6333982d67d9cf
Christopher Wavrin <wavrin@gmail.com>
2014-04-07 14:54:02 -0500

    added some authentication

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,7 @@
 class ArticlesController < ApplicationController
   before_action :set_article, only: [:show, :edit, :update, :destroy]
-
+  before_action :require_signin, except: [:index, :show]
+  
   # GET /articles
   # GET /articles.json
   def index
